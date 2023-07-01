7633747cd15746dab0314e09fb9245492b70c32d
Hp <Hp@DESKTOP-8HIKA16.(none)>
2023-07-01 13:44:14 +0530

    manage user with profile context put real time subs

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import PrivateRoute from './componenets/PrivateRoute';
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import PublicRoute from './componenets/PublicRoute';
+import { ProfileProvider } from './context/profile.context';
 
 function App() {
   return (
-    <Switch>
-      <PublicRoute path="/signin">
-        <SignIn />
-      </PublicRoute>
-      <PrivateRoute path="/">
-        <Home />
-      </PrivateRoute>
-    </Switch>
+    <ProfileProvider>
+      <Switch>
+        <PublicRoute path="/signin">
+          <SignIn />
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </ProfileProvider>
   );
 }
 
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 const config = {
   apiKey: 'AIzaSyB1e46KMCJMdKM4aQp-APrs1SaGqLdDBS4',
   authDomain: 'chat-web-app-a1f61.firebaseapp.com',
@@ -8,3 +10,5 @@ const config = {
   appId: '1:420405679067:web:ffd55ac4afe930c255cdfe',
 };
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
