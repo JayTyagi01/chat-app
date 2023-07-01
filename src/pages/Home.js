0e1c456817456e8451de31dbdb61394944f99728
Hp <Hp@DESKTOP-8HIKA16.(none)>
2023-07-01 14:17:43 +0530

    start building the sidebar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Col, Grid, Row } from 'rsuite';
+import Sidebar from '../componenets/Sidebar';
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <Grid fluid className="h-100">
+      <Row>
+        <Col xs={24} md={8}>
+          <Sidebar />
+        </Col>
+      </Row>
+    </Grid>
+  );
 };
 export default Home;
